3a42327bfbc93e22ca6e3d0342bf6478b480d1b9
Jona Koudijs <jona@koudijs.me>
2023-07-25 10:26:47 +0200

    Test failed build

---
+++ status.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+GAME="$1"
+CMD="curl -s https://hub.docker.com/api/build/v1/source/\?image=lanlords/$GAME | jq -r '.objects[0].state'"
+STARTED="0"
+
+while :
+do
+
+    if [ "$STARTED" == "0" ]
+    then
+
+        OUTPUT="$(eval $CMD)"
+        if [ "$OUTPUT" == "Building" ]
+        then
+            echo "[status: $OUTPUT] Docker Build has started! Waiting to finish."
+            STARTED="1"
+         else
+            echo "[status: $OUTPUT] Docker Build has not yet started."
+        fi
+
+    else
+
+        OUTPUT="$(eval $CMD)"
+        if [ "$OUTPUT" == "Success" ]
+        then
+        	echo "[status: $OUTPUT] Docker Build has finished!"
+            break
+        else
+            echo "[status: $OUTPUT] Docker Build is still running."
+        fi
+
+    fi
+
+    # wait for 5 seconds
+    sleep 5
+
+done
